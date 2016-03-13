c606a0bc84ff861d104bd12f33ee721731a098fb
Chen Peng <i@mail.chenpeng.info>
2016-03-12 21:21:56 -0500

    ChenPeng 2016-03-13 10:21:56

--- src/Provider/QQ.php
+++ src/Provider/QQ.php
@@ -26,10 +26,10 @@ class QQ extends AbstractProvider
 
     public function getResourceOwnerDetailsUrl(AccessToken $token)
     {
-        $this->openid = $this->getOpenid($token);
+        $this->openid = $this->requestOpenid($token);
 
         return 'https://graph.qq.com/user/get_user_info?' . http_build_query([
-            'access_token'       => $token,
+            'access_token'       => $token->getToken(),
             'oauth_consumer_key' => $this->clientId,
             'openid'             => $this->openid,
         ]);
@@ -38,24 +38,80 @@ class QQ extends AbstractProvider
     protected function getOpenidUrl(AccessToken $token)
     {
         return 'https://graph.qq.com/oauth2.0/me?' . http_build_query([
-            'access_token' => $token,
+            'access_token' => $token->getToken(),
         ]);
     }
 
-    protected function getOpenid(AccessToken $token)
+    protected function requestOpenid(AccessToken $token)
     {
         $url     = $this->getOpenidUrl($token);
         $request = $this->getRequest(self::METHOD_GET, $url);
         $data    = $this->getResponse($request);
-
-        if (strpos($data, 'callback') !== false)
+        $data    = $this->parse_jsonp($data);
+        if (!empty($data) && isset($data['openid']))
         {
-            $data = str_replace('callback( ', '', str_replace(' );', '', $data));
-            $data = json_decode($data, true);
             $data = $data['openid'];
         }
 
         return $data;
+    }
+
+    public function getOpenId()
+    {
+        return $this->openid;
+    }
+
+    public function getClientId()
+    {
+        return $this->clientId;
+    }
+
+    protected function parse_jsonp($jsonp_str)
+    {
+        if (strpos($jsonp_str, 'callback') !== false)
+        {
+            $json_str = str_replace('callback( ', '', str_replace(' );', '', $jsonp_str));
+            $data = json_decode($json_str, true);
+        }
+
+        return isset($data) ? $data : null;
+    }
+
+    /**
+     * Requests an access token using a specified grant and option set.
+     *
+     * @param  mixed $grant
+     * @param  array $options
+     * @return AccessToken
+     */
+    public function getAccessToken($grant, array $options = [])
+    {
+        $grant = $this->verifyGrant($grant);
+
+        $params = [
+            'client_id'     => $this->clientId,
+            'client_secret' => $this->clientSecret,
+            'redirect_uri'  => $this->redirectUri,
+        ];
+
+        $params   = $grant->prepareRequestParameters($params, $options);
+        $request  = $this->getAccessTokenRequest($params);
+        $response = $this->sendRequest($request);
+        $content  = (string)$response->getBody();
+        $parsed   = $this->parse_jsonp($content);
+        if (!empty($parsed))
+        {
+            $this->checkResponse($response, $parsed);
+        }
+        else
+        {
+            parse_str($content, $parsed);
+        }
+
+        $prepared = $this->prepareAccessTokenResponse($parsed);
+        $token    = $this->createAccessToken($prepared, $grant);
+
+        return $token;
     }
 
     protected function getDefaultScopes()
@@ -63,18 +119,13 @@ class QQ extends AbstractProvider
         return [];
     }
 
-    protected function getScopeSeparator()
-    {
-        return ' ';
-    }
-
     protected function checkResponse(ResponseInterface $response, $data)
     {
-        if (!empty($data['error']))
+        if (isset($data['error']))
         {
             throw new IdentityProviderException(
-                $data['error'] ?: $response->getReasonPhrase(),
-                $response->getStatusCode(),
+                $data['error_description'],
+                $data['error'],
                 $response
             );
         }
